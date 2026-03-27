0989878a195002b6bcbedfb2e9c0d7d77f460c5a
mairaquintanilla <mai.quintanilla@duocuc.cl>
2026-03-26 21:27:55 -0300

    final del codigo

---
+++ src/main/java/com/example/biblioteca/modelo/controller/LibroController.java
@@ -0,0 +1,55 @@
+package com.example.biblioteca.modelo.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.biblioteca.modelo.Libro;
+import com.example.biblioteca.sservice.LibroService;
+
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.PutMapping;
+
+
+
+
+@RestController
+@RequestMapping("/api/v1/libros")
+public class LibroController {
+
+    @Autowired
+    private LibroService libroService;
+
+    @GetMapping("")
+    public List<Libro> listarLibros() {
+        return libroService.getAllLibros();
+    }
+    @PostMapping("path")
+    public Libro agregarLibro(@RequestBody Libro libro){
+        return libroService.saveLibro(libro);
+    }
+    @GetMapping("path")
+    public Libro buscarLibro(@PathVariable int id){
+        return libroService.getlibroId(id);
+    }
+    @PutMapping("{id}")
+    public Libro actualizarLibro(@PathVariable int id, @RequestBody Libro libro){
+        // el id lo usaremos mas adelante
+        return libroService.updateLibro(libro);
+    }
+    @DeleteMapping
+    public String eliminarLibro(@PathVariable int id){
+        return libroService.deleteLibro(id);
+    }
+    
+    
+    
+
+}
--- src/main/java/com/example/biblioteca/sservice/LibroService.java
+++ src/main/java/com/example/biblioteca/sservice/LibroService.java
@@ -1,8 +1,33 @@
 package com.example.biblioteca.sservice;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import com.example.biblioteca.modelo.Libro;
+import com.example.biblioteca.repository.LibroRepository;
 
 @Service
 public class LibroService {
+    @Autowired
+    private LibroRepository libroRepository;
 
+    public List<Libro> getAllLibros() {
+        return libroRepository.obtenerLibros();
+    }
+    public Libro saveLibro(Libro libro){
+        return libroRepository.guardar(libro);
+    }
+    public Libro getlibroId(int id){
+        return libroRepository.buscarPorId(id);
+    }
+    public Libro updateLibro(Libro libro){
+        return libroRepository.actualizar(libro);
+    }
+    public String deleteLibro(Libro libro){
+        libroRepository.eliminar(id);
+        return "Producto eliminado";
+
+    }
 }
